593ff18177d0604d13f6a6562e8dd12d259a7049
jtfl2008 <jtfl2008@gmail.com>
2016-05-30 18:17:36 +0800

    nav,arrow 动态添加

--- js/slider.js
+++ js/slider.js
@@ -1,60 +1,66 @@
 // 轮播图
-;(function($){
-    $.fn.slider = function(options){
-        return this.each(function(){
-            var defaults={
-                event:"mouseenter",
-                autoplaySpeed:3000,
-                speed:300,
+;
+(function($) {
+    $.fn.slider = function(options) {
+        return this.each(function() {
+            var defaults = {
+                event: "mouseenter",
+                autoplaySpeed: 3000,
+                speed: 300,
+                arrows: true,
+                nav: true,
             }
-            var opts=$.extend({},defaults,options),
-                _this=$(this),
-                index=0,
+            var opts = $.extend({}, defaults, options),
+                _this = $(this),
+                index = 0,
                 timer,
-                slider_items=_this.find('.slider_items'),
+                slider_items = _this.find('.slider_items'),
 
-                slider_item=slider_items.find('li'),
-                len=slider_item.length,
-                slider_item_w=slider_item.outerWidth();
+                slider_item = slider_items.find('li'),
+                len = slider_item.length,
+                slider_item_w = slider_item.outerWidth();
+            if (opts.nav) {
+                _this.append('<ul class="navs"></ul>');
+            }
+            if (opts.arrows) {
+                _this.append('<div class="prev btn">&lt;</div><div class="next btn">&gt;</div>');
+            }
+            for (var i = 1; i <= len; i++) {
+                var li = "<li>" + i + "</li>";
+                var navs = _this.find(".navs").append(li);
+            }
+            _this.find(".navs li").eq(0).addClass("active");
 
-                for(var i=1;i<=len;i++){
-                    var li="<li>"+i+"</li>";
-                    var navs= _this.find(".navs").append(li);
-                }
-                    console.log(navs);
-                
-                // _this.append(navs);
-                _this.find('.navs li').eq(0).addClass('active');
+            slider_items.css({
+                width: len * slider_item_w
+            });
 
-                slider_items.css({
-                    width:len*slider_item_w
-                });
-            function change(index){
+            function change(index) {
                 var curLeft = -index * slider_item_w;
                 slider_items.stop().animate({
-                    left:curLeft
+                    left: curLeft
                 }, opts.speed);
 
                 _this.find('.navs li').eq(index).addClass('active').siblings().removeClass('active');
             };
 
-            _this.find('.prev').on("click",function(){
+            _this.find('.prev').on("click", function() {
                 index--;
-                if( index ==-1){
-                    index = len -1;
+                if (index == -1) {
+                    index = len - 1;
                 }
 
                 change(index);
             });
-            _this.find('.next').on("click",function(){
+            _this.find('.next').on("click", function() {
                 index++;
-                if( index ==len){
+                if (index == len) {
                     index = 0;
                 }
 
                 change(index);
             });
-            _this.on(opts.event,".navs li",function(){
+            _this.on(opts.event, ".navs li", function() {
                 var _index = $(this).index();
                 index = _index;
                 change(index);
@@ -62,13 +68,13 @@
             _this.hover(function() {
                 clearInterval(timer);
             }, function() {
-                timer = setInterval(function(){
-                    index ++;
-                    if(index == len){
-                        index=0;
+                timer = setInterval(function() {
+                    index++;
+                    if (index == len) {
+                        index = 0;
                     }
                     change(index);
-                },opts.autoplaySpeed)
+                }, opts.autoplaySpeed)
             }).trigger("mouseleave");
         })
     }
