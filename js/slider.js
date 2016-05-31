36826c9f92c97d3e7a1f28c061db20f8d72f5615
jtfl2008 <jtfl2008@gmail.com>
2016-05-31 15:31:56 +0800

    autoplay 参数添加

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@
         return this.each(function() {
             var defaults = {
                 event: "mouseenter",
+                autoplay:true,
                 autoplaySpeed: 3000,
                 speed: 300,
                 arrows: true,
@@ -17,8 +18,14 @@
                 slider_items = _this.find('.slider_items'),
 
                 slider_item = slider_items.find('li'),
+
                 len = slider_item.length,
-                slider_item_w = slider_item.outerWidth();
+                slider_item_w = slider_item.outerWidth() || $(window).width();
+                slider_item_h = slider_item.outerHeight() || $(window).height();
+            _this.css({
+                width: slider_item_w,
+                height: slider_item_h
+            });
             if (opts.nav) {
                 _this.append('<ul class="navs"></ul>');
             }
@@ -29,7 +36,8 @@
                 var li = "<li>" + i + "</li>";
                 var navs = _this.find(".navs").append(li);
             }
-            _this.find(".navs li").eq(0).addClass("active");
+            var navs_li=_this.find(".navs li");
+            navs_li.eq(0).addClass("active");
 
             slider_items.css({
                 width: len * slider_item_w
@@ -37,17 +45,16 @@
 
             // fade
             if (opts.fade) {
-                _this.addClass("fade");
-
                 function change_fade(index) {
-                    slider_item.eq(index).fadeIn().siblings().fadeOut();
+                    slider_item.eq(index).stop(true,true).fadeIn(opts.speed).siblings().fadeOut(opts.speed);
                     change_nav(index);
                 };
+
             } else {
             // slider
                 function change_slider(index) {
                     var curLeft = -index * slider_item_w;
-                    slider_items.stop().animate({
+                    slider_items.stop(true,true).animate({
                         left: curLeft
                     }, opts.speed);
 
@@ -55,53 +62,50 @@
 
                     _this.addClass('active').siblings().removeClass('active');
                 };
-                _this.find('.prev').on("click", function() {
-                    index--;
-                    if (index == -1) {
-                        index = len - 1;
-                    }
-
-                    change_slider(index);
-                });
-                _this.find('.next').on("click", function() {
-                    index++;
-                    if (index == len) {
-                        index = 0;
-                    }
-
-                    change_slider(index);
-                });
+                
             }
+            _this.find('.prev').on("click", function() {
+                index--;
+                if (index == -1) {
+                    index = len - 1;
+                }
+
+                change_slider(index);
+            });
+            _this.find('.next').on("click", function() {
+                index++;
+                if (index == len) {
+                    index = 0;
+                }
+
+                change_slider(index);
+            });
             // 导航
             function  change_nav(index){
-                _this.find('.navs li').eq(index).addClass('active').siblings().removeClass('active');
+                navs_li.eq(index).addClass('active').siblings().removeClass('active');
             }
             _this.on(opts.event, ".navs li", function() {
-
                 var _index = $(this).index();
                 index = _index;
-                if (opts.fade) {
-                    change_fade(index);
-                } else {
-                    change_slider(index);
-                }
+                opts.fade ? change_fade(index) : change_slider(index);
+
             });
             // 触发事件
-            _this.hover(function() {
+            if( opts.autoplay){
+                _this.hover(function() {
                 clearInterval(timer);
-            }, function() {
+                }, function() {
                 timer = setInterval(function() {
                     index++;
                     if (index == len) {
                         index = 0;
                     }
-                    if (opts.fade) {
-                        change_fade(index);
-                    } else {
-                        change_slider(index);
-                    }
+                   opts.fade ? change_fade(index) : change_slider(index)
                 }, opts.autoplaySpeed)
             }).trigger("mouseleave");
+
+            }
+            
         })
     }
 })(jQuery)
